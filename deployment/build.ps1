a2ed126fc4b5f35bdd57763cdc11567c51fb74a3
Matt Salmon <matt@iua.co.za>
2018-01-03 16:01:21 +0200

    Added deployment script

---
+++ deployment/build.ps1
@@ -0,0 +1,25 @@
+function ZipFile
+{
+	param(
+		[String]$sourceFile,
+		[String]$zipFile
+	)
+
+	set-alias sz "C:\Program Files\7-Zip\7z.exe"  
+	sz a -tzip -r $zipFile $sourceFile | Out-Null
+}
+
+$root = $PSScriptRoot
+$source = $root.Replace("deployment", "") + "\source"
+$version = Read-Host -Prompt "What version are we building?"
+
+# build for mongo db 
+Write-Host "Building Stateless.Workflow.MongoDB version $version"
+$binfolder = "$source\SqlServerReportRunner\bin\Release"
+[system.io.file]::Delete("$binfolder\Connections.config")
+$zip = "$root\Output\SqlServerReportRunner_v$version.zip"
+[system.io.file]::Delete($zip)
+ZipFile -sourcefile "$binfolder\*.*" -zipfile $zip 
+
+Write-Host "Done" -BackgroundColor Green -ForegroundColor White
+
